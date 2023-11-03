da1eb255e3cdc70f6cd9ca170d231482739d4696
cdrmack <git.pynvt@passmail.net>
2023-11-03 13:42:18 +0100

    add `lsh_read_line()`

--- main.c
+++ main.c
@@ -1,9 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+const int LSH_RL_BUFFERSIZE = 1024;
+
+char* lsh_read_line()
+{
+    int buffer_size = LSH_RL_BUFFERSIZE;
+    char* buffer = malloc(sizeof(char) * buffer_size);
+
+    if (!buffer)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+	exit(EXIT_FAILURE);
+    }
+
+    int position = 0;
+    while (true)
+    {
+        char c = getchar();
+
+	if (c == EOF || c == '\n')
+        {
+  	    buffer[position] = '\n';
+	    return buffer;
+        }
+	else
+        {
+  	    buffer[position] = c;
+	}
+	
+	++position;
+
+	if (position >= buffer_size)
+        {
+	    // TODO: realocate buffer if needed
+	}
+    }
+}
 
 void lsh_loop(void)
 {
-    // TODO
+    char* line;
+    int status = 1;
+
+    do
+    {
+        printf("> ");
+	line = lsh_read_line();
+        printf("read: %s", line); // TODO: remove
+
+	free(line); // allocated inside lsh_read_line
+    } while (status);
 }
 
 int main(int argc, char* argv[])
