f5c1fb2b1b2b6d4b5ba37f0fd6bbffdbc5f676e1
cdrmack <git.pynvt@passmail.net>
2023-11-03 13:26:07 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void lsh_loop(void)
+{
+    // TODO
+}
+
+int main(int argc, char* argv[])
+{
+    printf("lsh\n");
+    lsh_loop();
+
+    return EXIT_SUCCESS;
+}
