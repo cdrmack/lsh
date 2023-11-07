869fabd0454de0858691eaafafc4cef3800b9956
cdrmack <git.pynvt@passmail.net>
2023-11-07 11:55:43 +0100

    add `lsh_split_line()` to get list of arguments

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 const size_t LSH_RL_BUFFERSIZE = 1024;
+const size_t LSH_SL_BUFFERSIZE = 64;
+const char* LSH_SL_DELIM = " \t\r\n\a";
 
-char* lsh_read_line()
+char* lsh_read_line(void)
 {
     char* buffer = NULL;
     size_t buffer_size = 0;
@@ -26,18 +29,59 @@ char* lsh_read_line()
     return buffer;
 }
 
+/*
+ * No support for quoting or escaping.
+ */
+char** lsh_split_line(char* line)
+{
+    size_t buffer_size = LSH_SL_BUFFERSIZE;
+    char** tokens = (char**)malloc(buffer_size * sizeof(char*));
+
+    if (!tokens)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int position = 0;
+    char* token = strtok(line, LSH_SL_DELIM);
+    while (token)
+    {
+        tokens[position] = token;
+        ++position;
+
+        if (position >= buffer_size)
+        {
+            buffer_size += LSH_SL_BUFFERSIZE;
+            tokens = realloc(tokens, buffer_size * sizeof(char*));
+            if (!tokens)
+            {
+                fprintf(stderr, "lsh: reallocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_SL_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
+}
+
 void lsh_loop(void)
 {
-    char* line;
     int status = 1;
+    char* line = NULL;
+    char** arguments = NULL;
 
     do
     {
-	printf("> ");
-	line = lsh_read_line();
-	printf("read: %s", line); // TODO: remove
+        printf("> ");
+        line = lsh_read_line();
+        arguments = lsh_split_line(line);
 
-	free(line); // allocated inside lsh_read_line
+        free(line); // allocated inside lsh_read_line
+        free(arguments); // allocated inside lsh_split_line
     } while (status);
 }
 
